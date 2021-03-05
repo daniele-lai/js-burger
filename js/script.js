9ddce329eb745830970c0f5a2499aa2a29081951
Daniele <danielelai86@gmail.com>
2021-03-05 17:18:29 +0100

    Aggiunta parte finale js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
 /*Chiedere il nome del panino, selezionare gli ingredienti, calcolare la somma e applicare lo sconto*/
 
 var calculateOrder = document.getElementById('calculate');
-var somma = 50;
+var sale =["boolean20", "burger40", "spoiledcustomers", "ciao"];
 
 calculateOrder.addEventListener("click",
   function () {
     //Facciamo scegliere il nome del panino
     var burgerName = document.getElementById('burger_name');
     if (burgerName.value != "") {
+    //Selezioniamo gli ingredienti
+    var burgerExtra = document.getElementsByClassName('burger_stuff');
+    var somma = 50;
 
-      //Selezioniamo gli ingredienti
-      var burgerExtra = document.getElementsByClassName('burger_stuff');
       for (var i = 0; i < burgerExtra.length; i++) {
         if (burgerExtra[i].checked == true) {
-          somma += parseInt(burgerExtra[i].value);
-          console.log(somma);
-        }
+            somma += parseInt(burgerExtra[i].value);
+          }
       }
-
-
+      //Applichiamo uno sconto
+      var saleCoupon = document.getElementById('discount');
+      if (sale.includes(saleCoupon.value)) {
+        somma = somma - (somma * 20 / 100);
+      }
+      // Applichiamo la somma
+      var totalCheck = document.getElementById('sum');
+      totalCheck.innerHTML = somma.toFixed(2) + "€";
     } else {
       alert("Inserisci un nome per il tuo burger")
-    }
+      }
   }
 );
